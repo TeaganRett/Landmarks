c63f76980a6e19dbdd5ae4edc3e267738dd86f0c
TeaganRett <teagan.rett@gmail.com>
2021-01-05 08:17:46 -0600

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Tiger Coder on 1/5/21.
+//  Copyright © 2021 Tiger Coder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
